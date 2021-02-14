396415f79792829bbe82b2c7781f59de1d791c50
Seiji Ainoguchi <14823909+ain1084@users.noreply.github.com>
2021-02-14 18:03:54 +0900

    Simplify section_min_max.v logic.

--- src/section_min_max.v
+++ src/section_min_max.v
@@ -13,8 +13,7 @@ module section_min_max #(parameter width = 16, sample_count = 16)(
 
     reg [$clog2(sample_count)-1:0] count;
     wire [$clog2(sample_count)-1:0] count_last = sample_count - 1'b1;
-    reg state;
-    assign i_ready = state == 1'b0;
+    assign i_ready = !o_valid;
     
     always @(posedge clk or posedge reset) begin
         if (reset) begin
@@ -22,29 +21,18 @@ module section_min_max #(parameter width = 16, sample_count = 16)(
             count <= 0;
             o_max_value <= 0;
             o_min_value <= -1;
-            state <= 1'b0;
         end else begin
-            case (state)
-            1'b0:
-                if (i_valid && i_ready) begin
-                    o_max_value <= o_max_value < i_value ? i_value : o_max_value;
-                    o_min_value <= o_min_value > i_value ? i_value : o_min_value;
-                    if (count == count_last) begin
-                        count <= 1'b0;
-                        state <= 1'b1;
-                    end else
-                        count <= count + 1'b1;
-                end
-            1'b1:
-                if (!o_valid) begin
+            if (i_valid && i_ready) begin
+                o_max_value <= o_max_value < i_value ? i_value : o_max_value;
+                o_min_value <= o_min_value > i_value ? i_value : o_min_value;
+                count <= (count == count_last) ? 1'b0 : count + 1'b1;
+                if (count == count_last)
                     o_valid <= 1'b1;
-                end else if (o_ready) begin
-                    o_max_value <= 0;
-                    o_min_value <= -1;
-                    o_valid <= 1'b0;
-                    state <= 1'b0;
-                end
-            endcase
+            end else if (o_valid && o_ready) begin
+                o_max_value <= 0;
+                o_min_value <= -1;
+                o_valid <= 1'b0;
+            end
         end
     end
 endmodule
